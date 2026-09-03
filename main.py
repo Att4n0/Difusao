f398656370c6f0e081499b4a192824d38cceb801
João Paulo Lunkes <joaopaulolunkes@gmail.com>
2026-09-03 13:29:17 -0300

    Added docstring, remade the substance search function so that it returns data instead of just printing information, added the option to input more than one substance at a time.

--- main.py
+++ main.py
@@ -2,28 +2,52 @@ import os
 import subprocess
 import pandas as pd
 
-dados = pd.read_csv(
-    "dados/tabelak2.csv",
-    sep=";",
-    decimal="."
-)
-
 def limpa_tela():
+    '''Limpa a tela, escolhendo o método pelo OS do usuário.'''
     if os.name == 'nt':
         subprocess.run('cls', shell=True)
     else:
         subprocess.run(['clear'])
 
-def encontrar_substancia():
-    nome = input("Digite o nome da substância: ").strip().lower()
+def encontrar_substancia(df,nome):
+    '''Retorna os dados de uma substância
+    
+    Inputs
+    - Nome da substância
+    - Nome do arquivo da busca
 
-    substancia = dados[dados["nome"].str.strip().str.lower() == nome]
+    Outputs
+    -None, caso não o nome passado não seja encontrado no arquivo
+    -Retorno das informações da substância no formato pandas.Series
+    '''
+    
+    termo = nome.strip().lower()
+    substancia = df[df["nome"].str.strip().str.lower() == termo]
 
     if substancia.empty:
-        print("Substância não encontrada.")
+        return None
     else:
-        print(substancia)
+        return(substancia)
 
 if __name__ == '__main__':
     limpa_tela()
-    encontrar_substancia()
+
+    dadosk2 = pd.read_csv("dados/tabelak2.csv", sep=";", decimal=".")
+
+    nome_soluto = input('Digite o nome do soluto: ')
+    soluto = encontrar_substancia(dadosk2, nome_soluto)
+
+    nome_solvente = input('\nAgora, digite o nome do solvente: ')
+    solvente = encontrar_substancia(dadosk2, nome_solvente)
+
+    print('--- BUSCA DE COMPONENTES ---')
+
+    if soluto is None:
+        print(f"Erro: Soluto '{nome_soluto}' não foi encontrado no banco de dados.")
+
+    if solvente is None:
+        print(f"Erro: Solvente '{nome_solvente}' não foi encontrado no banco de dados.")
+        
+    if soluto is not None and solvente is not None:
+        print(f'\nDados do soluto:\n{soluto}')
+        print(f'\nDados od solvente:\n{solvente}')
