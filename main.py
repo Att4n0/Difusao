a69ee83fcbf69d7b671b7b2d40793cdca8a56581
João Paulo Lunkes <joaopaulolunkes@gmail.com>
2026-09-03 15:25:24 -0300

    Responsibility for managing the database and storing information on the substances was transferred to the respective classes.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import pandas as pd
+from modelos import DataBase, Substancia
 
 def limpa_tela():
     '''Limpa a tela, escolhendo o método pelo OS do usuário.'''
@@ -9,45 +10,23 @@ def limpa_tela():
     else:
         subprocess.run(['clear'])
 
-def encontrar_substancia(df,nome):
-    '''Retorna os dados de uma substância
-    
-    Inputs
-    - Nome da substância
-    - Nome do arquivo da busca
-
-    Outputs
-    -None, caso não o nome passado não seja encontrado no arquivo
-    -Retorno das informações da substância no formato pandas.Series
-    '''
-    
-    termo = nome.strip().lower()
-    substancia = df[df["nome"].str.strip().str.lower() == termo]
-
-    if substancia.empty:
-        return None
-    else:
-        return(substancia)
-
 if __name__ == '__main__':
     limpa_tela()
 
-    dadosk2 = pd.read_csv("dados/tabelak2.csv", sep=";", decimal=".")
+    tabelak2 = DataBase("Propriedades", "dados/tabelak2.csv", sep=";", decimal=".")
+    tabelak1 = DataBase("IntegralColisao", "dados/tabelak1.csv", sep=";", decimal=",")
 
-    nome_soluto = input('Digite o nome do soluto: ')
-    soluto = encontrar_substancia(dadosk2, nome_soluto)
+    nome_soluto = input("Digite o nome do soluto: ")
+    nome_solvente = input("Digite o nome do solvente: ")
 
-    nome_solvente = input('\nAgora, digite o nome do solvente: ')
-    solvente = encontrar_substancia(dadosk2, nome_solvente)
+    soluto = tabelak2.buscar_substancia(nome_soluto)
+    solvente = tabelak2.buscar_substancia(nome_solvente)
 
-    print('--- BUSCA DE COMPONENTES ---')
+    if soluto is not None and solvente is not None:
+        print(f"\nSoluto encontrado: {soluto.nome} (Fórmula molecular = {soluto.formula}) (M = {soluto.massa_molar} g/mol)")
+        print(f"Solvente encontrado: {solvente.nome} (Fórmula molecular = {solvente.formula}) (M = {solvente.massa_molar} g/mol)")
 
     if soluto is None:
-        print(f"Erro: Soluto '{nome_soluto}' não foi encontrado no banco de dados.")
-
+       print(f"Erro: Soluto '{nome_soluto}' não foi encontrado no banco de dados.")
     if solvente is None:
-        print(f"Erro: Solvente '{nome_solvente}' não foi encontrado no banco de dados.")
-        
-    if soluto is not None and solvente is not None:
-        print(f'\nDados do soluto:\n{soluto}')
-        print(f'\nDados od solvente:\n{solvente}')
+       print(f"Erro: Solvente '{nome_solvente}' não foi encontrado no banco de dados.")
